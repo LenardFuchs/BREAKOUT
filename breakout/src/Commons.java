ca906cda2a1b5593c77cef2127dea1cb8979b6c0
Janella Gatmaitan <janella.gatmaitan@gmail.com>
2023-01-23 12:04:38 +0100

    Brought up Paddle and Ball

--- breakout/src/Commons.java
+++ breakout/src/Commons.java
@@ -8,7 +8,7 @@ public interface Commons { //interface, like a container for all common constant
         //int N_OF_OBSTACLES;
         int INIT_PADDLE_X = WIDTH/2-60; //200*3; //starting position of paddle in X-coordinate
         int INIT_PADDLE_Y = 700; //starting position of paddle in Y-coordinate
-        int INIT_BALL_X = WIDTH/2-23;//230*3; //starting position of ball in X-coordinate
+        int INIT_BALL_X = WIDTH/2-23; //230*3; //starting position of ball in X-coordinate
         int INIT_BALL_Y = 655;//355*2; //starting position of ball in Y-coordinate
         int PERIOD = 4; // time in ms between task executions
 }
